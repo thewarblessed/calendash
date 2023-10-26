1d4453e7e410426eea167c3d512d8a146f404a77
unknown <coolxfamous@gmail.com>
2023-10-26 22:43:02 +0800

    second commit

---
+++ database/migrations/2023_10_22_064544_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('tupID');
+            $table->text('lastname');
+            $table->text('firstname');
+            $table->text('course');
+            $table->text('studOrg');
+            $table->text('yearlevel');
+            $table->text('section');
+            $table->string('image')->default('customer.jpg');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
